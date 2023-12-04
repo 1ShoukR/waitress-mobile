66e737d9532dcd7da545219bd8e8b358b68180ef
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-12-04 16:14:42 -0500

    adjusted theme of application

--- constants/theme.js
+++ constants/theme.js
@@ -9,65 +9,61 @@ the webapp style/color scheme.
 */
 
 const COLORS = {
-	primary: '#4C4ADD',
-	secondary: '#1B2D4B',
-	success: '#37C674',
-	info: '#6765E2',
-	danger: '#FE5050',
-	warning: '#FE9700',
+	primary: '#D23D2B', // Warm Red
+	secondary: '#8C9A5E', // Olive Green
+	success: '#4CAE50', // Fresh Green
+	info: '#FFC107', // Golden Yellow
+	danger: '#C62828', // Deep Red
+	warning: '#FFA726', // Orange
 
-	gray100: '#F9F9FB',
-	gray200: '#E7E9EF',
-	gray300: '#C2C7D6',
-	gray400: '#919AB6',
-	gray500: '#6C799D',
-	gray600: '#525C7A',
-	gray700: '#394056',
-	gray800: '#212431',
-	gray900: '#101218',
+	gray100: '#F5F3EF', // Light Cream Beige
+	gray200: '#E8E0DA', // Warm Light Gray
+	gray300: '#D0C8BF', // Warm Gray
+	gray400: '#B8B0A6', // Medium Warm Gray
+	gray500: '#A0988E', // Darker Warm Gray
+	gray600: '#888077', // Dark Warm Gray
+	gray700: '#706A61', // Darker Warm Gray
+	gray800: '#58524A', // Almost Black Warm Gray
+	gray900: '#403C34', // Very Dark Warm Gray
 
-	status1: '#18BCD4',
-	status2: '#FD3772',
-	status3: '#91D54A',
-	status4: '#818FFF',
-	status5: '#18AAF2',
-	status6: '#FE8412',
-	tags: '#6C799D', // Equal to gray500
+	status1: '#4CAE50', // Status Fresh Green
+	status2: '#FFC107', // Status Golden Yellow
+	status3: '#D23D2B', // Status Warm Red
+	status4: '#FFA726', // Status Orange
+	status5: '#C62828', // Status Deep Red
+	status6: '#8C9A5E', // Status Olive Green
+	tags: '#A0988E', // Equal to gray500
 
-	/*
-      Convenient aliases for theme colors
-  */
-	tertiary: '#525C7A', // Equal to gray600
-	black: '#000000',
-	white: '#FFFFFF',
-	lightWhite: '#F9F9FB', // Equal to gray100
-	lightGray: '#E7E9EF', // Equal to gray200
-	gray2: '#C2C7D6', // Equal to gray300
-	gray: '#919AB6', // Equal to gray400
+	// Convenient aliases for theme colors
+	tertiary: '#888077', // Equal to gray600
+	black: '#000000', // Black
+	white: '#FFFFFF', // White
+	lightWhite: '#F5F3EF', // Equal to gray100
+	lightGray: '#E8E0DA', // Equal to gray200
+	gray2: '#D0C8BF', // Equal to gray300
+	gray: '#B8B0A6', // Equal to gray400
 
-	/*
-      Convenient definitions for commonly used variations of theme colors 
-  */
-	primaryDark: '#2624BC',
-	primaryLight: '#DBDBF8',
-	secondaryDark: '#182943',
-	secondaryBackground: '#1B2D4B33',
-	successDark: '#31B268',
-	successBackground: '#37C67433',
-	successBadgeDark: '#268A51',
-	successBadgeBackground: '#37C67426',
-	infoDark: '#39378B',
-	infoBackground: '#6765E233',
-	dangerDark: '#FE2F2F',
-	dangerBackground: '#FE505033',
-	warningDark: '#E58800',
-	warningBackground: '#FE970033',
-	draggableHoverBackground: '#F6F6FE',
-	draggableHoverIcon: '#2624BC', // Equal to primaryDark
-	linkText: '#6765E2', // Equal to info
-	mutedText: '#6C799D', // Equal to gray500
-	inputPlaceholder: '#919AB6', // Equal to gray400
-	cardBorder: '#E7E9EF', // Equal to gray200
+	// Convenient definitions for commonly used variations of theme colors
+	primaryDark: '#A53020', // Darker Shade of Warm Red
+	primaryLight: '#FFF5E5', // Light Cream
+	secondaryDark: '#6B7B47', // Dark Olive
+	secondaryBackground: '#8C9A5E33', // Light Olive Green
+	successDark: '#3E8E41', // Dark Fresh Green
+	successBackground: '#4CAE5033', // Fresh Green Light
+	successBadgeDark: '#357A38', // Darker Fresh Green
+	successBadgeBackground: '#4CAE5026', // Fresh Green Lighter
+	infoDark: '#CC9A06', // Dark Golden Yellow
+	infoBackground: '#FFC10733', // Light Golden Yellow
+	dangerDark: '#B21F1F', // Darker Deep Red
+	dangerBackground: '#C6282833', // Light Deep Red
+	warningDark: '#CC7A25', // Darker Orange
+	warningBackground: '#FFA72633', // Light Orange
+	draggableHoverBackground: '#FFF5E5', // Equal to primaryLight
+	draggableHoverIcon: '#A53020', // Equal to primaryDark
+	linkText: '#FFC107', // Equal to info
+	mutedText: '#A0988E', // Equal to gray500
+	inputPlaceholder: '#B8B0A6', // Equal to gray400
+	cardBorder: '#E8E0DA', // Equal to gray200
 };
 
 const FONT = {
