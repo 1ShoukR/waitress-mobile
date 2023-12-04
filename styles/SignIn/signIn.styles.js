ca3d18503aee55cac774048b62a72506e589fcb1
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-12-04 00:05:18 -0500

    added style page for sign in component

---
+++ styles/SignIn/signIn.styles.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from 'react-native';
+import { COLORS, FONT, SIZES, SHADOWS } from '../../constants';
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 16,
+		paddingTop: 96,
+		width: 400,
+	},
+	branding: {
+		color: COLORS.primary,
+		fontFamily: FONT.branding,
+		fontSize: 36,
+		marginBottom: SIZES.xxLarge,
+		paddingTop: 16,
+		textAlign: 'center',
+	},
+	title: {
+		color: COLORS.secondary,
+		fontSize: SIZES.xLarge,
+		fontWeight: 600,
+		marginBottom: SIZES.medium,
+	},
+	inputContainer: {
+		marginBottom: SIZES.medium,
+	},
+	input: {
+		//color: COLORS.gray800,
+		marginBottom: SIZES.small,
+		//outlineColor: COLORS.primary,
+		overflow: 'hidden',
+		//backgroundColor: COLORS.white,
+	},
+	button: {
+		marginBottom: 16,
+		paddingVertical: 8,
+	},
+});
+
+export default styles;
