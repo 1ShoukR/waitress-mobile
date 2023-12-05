f6c26b5b6fe55b023c1a029a387271a03ee01f5c
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-12-04 23:54:06 -0500

    adjusted the signin route to have a sign up button as well

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Pressable } from 'react-native'
 import React, { useCallback, useEffect, useState } from 'react';
 import { useRouter } from 'expo-router';
 import { TextInput, Button, Title } from 'react-native-paper';
 import styles from '../styles/SignIn/signIn.styles';
 // import { useDispatch, useSelector } from 'react-redux';
 import { COLORS } from '../constants';
+import Server from '../assets/server.svg'
 import Waiter from '../assets/Waiter.svg';
 import PressableButton from './PressableButton';
 // import { doLogin } from '../redux/thunks';
@@ -42,7 +43,7 @@ const SignIn = () => {
 			<View style={styles.headerContainer}>
 				<Title style={styles.branding}>Waitress</Title>
 				<View style={styles.brandingContainer}>
-					<Waiter width={90} height={80} />
+					<Server style={{ top: 9 }} width={90} height={60} />
 				</View>
 			</View>
 			<Title style={styles.title}>Login</Title>
@@ -74,6 +75,19 @@ const SignIn = () => {
 				/>
 			</View>
 			<PressableButton title="sign in" fontSize={18} theme="primary" loadingDelay={1000} />
+			<View style={{flex: 1, flexDirection: 'row'}}>
+				<Text style={styles.footerText}>Don't have an account?</Text>
+				<Pressable
+					style={({ pressed }) => [
+						{
+							backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+							padding: 5,
+							bottom: 7
+						},
+					]}>
+					<Text style={{textDecorationLine: 'underline', fontStyle: 'italic'}}>Sign up</Text>
+				</Pressable>
+			</View>
 		</View>
 	);
 };
