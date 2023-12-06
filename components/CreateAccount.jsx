32634352b5961040038de33cc06f79cf15be9551
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-12-05 21:30:34 -0500

    created a new create account component

--- components/CreateAccount.jsx
+++ components/CreateAccount.jsx
@@ -1,14 +1,14 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-export class CreateAccount extends Component {
-  render() {
-    return (
-      <View>
-        <Text>CreateAccount</Text>
-      </View>
-    )
-  }
+const CreateAccount = () => {
+  return (
+    <View>
+      <Text>CreateAccount</Text>
+    </View>
+  )
 }
 
 export default CreateAccount
+
+const styles = StyleSheet.create({})
