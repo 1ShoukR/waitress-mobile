3c58452d217f0222bcb4e936592728006b53a661
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-12-10 03:43:45 -0500

    created basic create account page

---
+++ app/account/create.js
@@ -0,0 +1,32 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import { COLORS } from '../../constants';
+import CreateAccountComponent from '../../components/CreateAccountComponent';
+
+const CreateAccount = () => {
+	return (
+		<>
+		<Stack.Screen
+		options={{
+			headerTitle: 'Create Your Account',
+			headerShown: true,
+			headerTintColor: COLORS.white,
+			headerShadowVisible: false,
+			headerStyle: {
+				backgroundColor: COLORS.primary
+			}
+		}}
+		/>
+		<SafeAreaView style={{ flex: 1, backgroundColor: COLORS.primary }}>
+			<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+				<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+					<CreateAccountComponent />
+				</View>
+			</View>
+		</SafeAreaView>
+		</>
+	);
+};
+
+export default CreateAccount;
