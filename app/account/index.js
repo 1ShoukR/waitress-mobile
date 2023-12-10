f1def957e5f2e9e93cae03bf75d439f0228e7dd8
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-12-10 03:43:12 -0500

    created a /account blank index page

---
+++ app/account/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+/*
+This page could be the basics for an account page if needed.
+This would just be /account
+*/
+
+const AccountIndex = () => {
+	return (
+		<View>
+			<Text>Account Page</Text>
+		</View>
+	);
+};
+
+export default AccountIndex;
