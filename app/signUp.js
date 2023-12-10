127bd1b29990d8b2b7c73aca3e79e99e50474f15
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-12-10 03:41:05 -0500

    adjusted header and button for signUp page

--- app/signUp.js
+++ app/signUp.js
@@ -3,22 +3,29 @@ import { View, ScrollView, LogBox, Button, Text, Pressable } from 'react-native'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
 import { Stack, router } from 'expo-router';
+import { Provider } from 'react-redux';
 import SignIn from '../components/SignIn';
+import store from '../redux/store';
+import { COLORS } from '../constants';
 
 const signUp = () => {
     const isPresented = router.canGoBack();
+	
     return (
-			<ScrollView showsVerticalScrollIndicator={false} style={{ backgroundColor: 'white' }}>
+			<ScrollView showsVerticalScrollIndicator={false} style={{ backgroundColor: COLORS.primary }}>
 				<Stack.Screen
 					options={{
 						headerShown: true,
 						headerTitle: 'Sign In',
 						headerShadowVisible: false,
+						headerStyle: {
+							backgroundColor: COLORS.primary
+						},
 						headerLeft: () => (
 							<Pressable
 								style={({ pressed }) => [
 									{
-										backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+										backgroundColor: pressed ? 'rgb(210, 230, 255)' : COLORS.primary,
 										padding: 10,
 									},
 								]}
