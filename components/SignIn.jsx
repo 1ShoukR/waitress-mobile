0e5a461d4a58fb135217807501e07f13ded5cde0
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-12-10 03:46:40 -0500

    restyled sign in page and added functionality to page

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -1,14 +1,13 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native'
+import { StyleSheet, Text, View, Pressable, Image } from 'react-native'
 import React, { useCallback, useEffect, useState } from 'react';
 import { router } from 'expo-router';
 import { TextInput, Button, Title } from 'react-native-paper';
 import styles from '../styles/SignIn/signIn.styles';
-// import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { COLORS } from '../constants';
-import Server from '../assets/server.svg'
-import Waiter from '../assets/Waiter.svg';
+import logo from '../assets/logo.png'
 import PressableButton from './PressableButton';
-// import { doLogin } from '../redux/thunks';
+import { doLogin } from '../redux/thunk';
 const SignIn = () => {
 	//useState Hooks
 	const [email, setEmail] = useState('');
@@ -17,42 +16,48 @@ const SignIn = () => {
 	//Expo Router Navigation
 
 	// // Redux Hooks
-	// const dispatch = useDispatch();
-	// const authStatus = useSelector((state) => state?.auth?.status);
+	const dispatch = useDispatch();
+	const authStatus = useSelector((state) => state?.auth?.status);
 
 	// Local Functions
-	// const handleLogin = useCallback(() => {
-	// 	dispatch(doLogin({ email: email, password: password }));
-	// }, [email, password, dispatch]);
+	const handleLogin = useCallback(() => {
+		dispatch(doLogin({ email: email, password: password }));
+	}, [email, password, dispatch]);
 
-	// useEffect(() => {
-	// 	if (authStatus === 'failed') {
-	// 		// TODO: Implement form validation
-	// 		// alert('Login failed. Please try again.');
-	// 		alert(t('common:login_failed'));
-	// 	} else if (authStatus === 'succeeded') {
-	// 		router.push('/horses');
-	// 		setEmail('');
-	// 		setPassword('');
-	// 	}
-	// }, [authStatus]);
+	useEffect(() => {
+		if (authStatus === 'failed') {
+			// TODO: Implement form validation
+			alert('Login failed. Please try again.');
+		} else if (authStatus === 'succeeded') {
+			router.push('/home');
+			setEmail('');
+			setPassword('');
+		}
+	}, [authStatus]);
+	const navigateToCreateAccount = () => {
+		router.back();
+		setTimeout(() => {
+			router.push('/account/create');
+		}, 100); 
+	};
 
 	return (
-		<View style={styles.container}>
+		<View style={[styles.container, { backgroundColor: COLORS.primary }]}>
 			<View style={styles.headerContainer}>
 				<Title style={styles.branding}>Waitress</Title>
 				<View style={styles.brandingContainer}>
-					<Server style={{ top: 9 }} width={90} height={60} />
+					<Image style={{ width: 35, height: 35, top: 10, marginLeft: 5 }} source={logo} />
 				</View>
 			</View>
 			<Title style={styles.title}>Login</Title>
 			<View style={styles.inputContainer}>
 				<TextInput
-					label="email"
+					label="Email"
+					placeholder='Enter your email'
 					value={email}
 					onChangeText={(text) => setEmail(text)}
-					mode="outlined"
-					activeOutlineColor={COLORS.primary}
+					mode="flat"
+					activeOutlineColor={COLORS.secondary}
 					outlineColor={COLORS.gray800}
 					textColor={COLORS.gray800}
 					style={styles.input}
@@ -61,31 +66,32 @@ const SignIn = () => {
 				<TextInput
 					label="Password"
 					value={password}
+					placeholder='Enter your password'
 					onChangeText={(text) => setPassword(text)}
 					secureTextEntry
 					right={<TextInput.Icon name="eye" />}
-					mode="outlined"
+					mode="flat"
 					activeOutlineColor={COLORS.primary}
 					outlineColor={COLORS.gray800}
-					textColor={COLORS.gray800}
+					textColor={COLORS.tertiary}
 					style={styles.input}
-					// onSubmitEditing={handleLogin}
+					onSubmitEditing={handleLogin}
 					returnKeyType="go"
 				/>
 			</View>
-			<PressableButton title="sign in" fontSize={18} theme="primary" loadingDelay={1000} />
-			<View style={{flex: 1, flexDirection: 'row'}}>
+			<PressableButton onPress={() => console.log('sign in')} title="sign in" fontSize={18} theme="primary" loadingDelay={1000} shadowStyle={{shadowOffset: { width: 0, height: 2 },shadowOpacity: 0.4,shadowRadius: 2.41,elevation: 2}} />
+			<View style={{ flex: 1, flexDirection: 'row' }}>
 				<Text style={styles.footerText}>Don't have an account?</Text>
 				<Pressable
-				onPress={() =>router.push('/createAccount')}
+					onPress={navigateToCreateAccount}
 					style={({ pressed }) => [
 						{
-							backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+							backgroundColor: pressed ? 'rgb(210, 230, 255)' : COLORS.primary,
 							padding: 5,
-							bottom: 7
+							bottom: 6,
 						},
 					]}>
-					<Text style={{textDecorationLine: 'underline', fontStyle: 'italic'}}>Sign up</Text>
+					<Text style={{ textDecorationLine: 'underline', fontStyle: 'italic' }}>Sign up</Text>
 				</Pressable>
 			</View>
 		</View>
