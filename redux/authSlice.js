5216606f76b7f72c46856255180f891ca2de1bd0
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-12-10 03:44:21 -0500

    added a redux auth slice

---
+++ redux/authSlice.js
@@ -0,0 +1,82 @@
+/**
+ * Auth slice
+ */
+import { createSlice } from '@reduxjs/toolkit';
+import { doLogin } from './thunk';
+
+const initialState = {
+	apiToken: null,
+	authType: null,
+	userId: null,
+	firstName: null,
+	lastName: null,
+	email: null,
+	userType: null,
+	status: 'idle',
+	error: null,
+	lastTab: 'horses',
+};
+
+const authSlice = createSlice({
+    name: 'auth',
+	initialState,
+	reducers: {
+        loggedOut: (state) => {
+			state.status = 'idle';
+			state.apiToken = null;
+			state.userId = null;
+			state.firstName = null;
+			state.lastName = null;
+			state.email = null;
+			state.userType = null;
+			state.authType = null;
+		},
+        /*
+        In theory, this is how we will determine what 
+        tab is active when we switch between tabs.
+        That way we can make sure the user stays on the 
+        same tab when they refresh the page.
+        -------------------------------------------------
+        someTabButton: (state) => {
+                state.lastTab = 'some tab name';
+            },
+            someTabButton: (state) => {
+                    state.lastTab = 'some tab name';
+                },
+         */
+	},
+	extraReducers(builder) {
+		builder
+			.addCase(doLogin.pending, (state, action) => {
+				state.status = 'loading';
+			})
+			.addCase(doLogin.fulfilled, (state, action) => {
+				const payload = action.payload;
+				if (payload && payload.token && payload.user) {
+					state.apiToken = payload.token;
+					state.userId = payload.user.user_id;
+					state.firstName = payload.user.first_name;
+					state.lastName = payload.user.last_name;
+					state.email = payload.user.email;
+					state.userType = payload.user.type;
+					state.authType = payload.user.auth_type;
+					state.status = 'succeeded';
+					// Add additional logic if needed for authgroup based on userType or authType
+				} else {
+					state.status = 'failed';
+					state.error = 'Login failed. Please try again.';
+				}
+			})
+			.addCase(doLogin.rejected, (state, action) => {
+				state.status = 'failed';
+				state.error = action.error.message;
+			});
+	},
+});
+
+export const { loggedOut } = authSlice.actions;
+export default authSlice.reducer;
+
+export const selectApiToken = (state) => state.auth.apiToken;
+// TODO: Add more selectors for new state fields like firstName, lastName, etc.
+
