151ad4fe1c69cf1383246649c64ef01c99d8e5bd
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-12-12 16:46:25 -0500

    made the basic page for the home screen of application

---
+++ app/home/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, ScrollView, LogBox, StyleSheet, SafeAreaView, Text } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { Button } from 'react-native-paper';
+import { COLORS } from '../../constants';
+
+
+
+
+
+
+
+
+
+const Home = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Home Screen Post login</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+
+
+export default Home
