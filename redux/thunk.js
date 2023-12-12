e06e7f8062625196be28350b1dc22f21c70a3517
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-12-12 16:47:15 -0500

    added user agent to formdata for posting

--- redux/thunk.js
+++ redux/thunk.js
@@ -8,6 +8,7 @@ export const doLogin = createAsyncThunk('auth/doLogin', async (requestData) => {
 	const formData = new FormData();
 	formData.append('email', requestData.email);
 	formData.append('password', requestData.password);
-	const data = await client.post('/auth/mobile/login', formData, null, { headers: { redirect: 'follow', referrerPolicy: 'no-referrer' } });
+	formData.append('user_agent', requestData.user_agent)
+	const data = await client.post('/auth/login', formData, null, { headers: { redirect: 'follow', referrerPolicy: 'no-referrer' } });
 	return data;
 });
