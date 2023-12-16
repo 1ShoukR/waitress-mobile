0eb0dd536677724138f60a8d6ba7aa5b2bcbd05c
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-12-15 20:34:38 -0500

    created a new thunk to handle account creation

--- redux/thunk.js
+++ redux/thunk.js
@@ -12,3 +12,14 @@ export const doLogin = createAsyncThunk('auth/doLogin', async (requestData) => {
 	const data = await client.post('/auth/login', formData, null, { headers: { redirect: 'follow', referrerPolicy: 'no-referrer' } });
 	return data;
 });
+
+export const createUserAccountThunk = createAsyncThunk('user/createUserAccountThunk', async (requestData) => {
+	const formData = new FormData()
+	formData.append('first_name', requestData.firstName)
+	formData.append('last_name', requestData.lastName)
+	formData.append('email', requestData.email)
+	formData.append('user_type', requestData.userType)
+	formData.append('password', requestData.password)
+	const data = await client.post('/user/create', formData, null, { headers: { redirect: 'follow', referrerPolicy: 'no-referrer' } })
+	return data
+});
