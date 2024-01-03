b0dae3e98ca31968918e74c59f20fd4b72428232
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-01-02 21:05:29 -0500

    added SearchListComponent

---
+++ components/SearchListComponent.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SearchListComponent = ({listItem}) => {
+    console.log('item', listItem);
+  return (
+    <View>
+      <Text>SearchListComponent</Text>
+    </View>
+  )
+}
+
+export default SearchListComponent
