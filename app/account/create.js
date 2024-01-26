6e4e72b483e351d414b1984553f9a44a3993f344
Rahmin <rahminshoukoohi@gmail.com>
2024-01-26 00:14:39 -0500

    fixed issue with account page not displaying

--- app/account/create.js
+++ app/account/create.js
@@ -4,29 +4,29 @@ import { View, Text, SafeAreaView } from 'react-native';
 import { COLORS } from '../../constants';
 import CreateAccountComponent from '../../components/CreateAccountComponent';
 
-const CreateAccount = () => {
+const createScreen = () => {
 	return (
 		<>
-		<Stack.Screen
-		options={{
-			headerTitle: 'Create Your Account',
-			headerShown: true,
-			headerTintColor: COLORS.white,
-			headerShadowVisible: false,
-			headerStyle: {
-				backgroundColor: COLORS.primary
-			}
-		}}
-		/>
-		<SafeAreaView style={{ flex: 1, backgroundColor: COLORS.primary }}>
-			<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-				<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-					<CreateAccountComponent />
+			<Stack.Screen
+				options={{
+					headerTitle: 'Create Your Account',
+					headerShown: true,
+					headerTintColor: COLORS.white,
+					headerShadowVisible: false,
+					headerStyle: {
+						backgroundColor: COLORS.primary,
+					},
+				}}
+			/>
+			<SafeAreaView style={{ flex: 1, backgroundColor: COLORS.primary }}>
+				<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+					<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+						<CreateAccountComponent />
+					</View>
 				</View>
-			</View>
-		</SafeAreaView>
+			</SafeAreaView>
 		</>
 	);
 };
 
-export default CreateAccount;
+export default createScreen;
