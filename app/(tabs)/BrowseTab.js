fe56f4e47d7cd6b22bf5ec188e025566f8bbbdfa
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-02-10 00:53:53 -0500

    created a basic browse page

--- app/(tabs)/BrowseTab.js
+++ app/(tabs)/BrowseTab.js
@@ -1,7 +1,17 @@
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import {Text, View} from 'react-native'
+import React, {useEffect} from "react"
+import { useDispatch } from "react-redux";
+import { setLastTab } from "../../redux/authSlice"; 
 
 const BrowseIndex = () => {
+    const dispatch = useDispatch()
+    const handleTabChange = (tabName) => {
+        dispatch(setLastTab(tabName));
+        };
+    useEffect(() => {
+        handleTabChange(tabName = 'BrowsTab')
+    })
     return (
         <SafeAreaProvider>
             <View>
