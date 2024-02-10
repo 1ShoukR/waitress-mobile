c7c4404dc0f6c1af9de489482a07b91cb4348df9
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-02-10 00:42:58 -0500

    created new category component

---
+++ components/CategoriesComponent.jsx
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import React from 'react'
+
+const CategoriesComponent = ({foodCategories}) => {
+  return (
+		<>
+			<Text style={styles.header}>Category</Text>
+			<ScrollView horizontal={true}>
+				<View style={styles.genreWrapper}>
+					{foodCategories.map((category, index) => (
+						<Text key={index} style={styles.genre}>
+							{category}
+						</Text>
+					))}
+				</View>
+			</ScrollView>
+		</>
+	);
+}
+
+export default CategoriesComponent
+
+const styles = StyleSheet.create({
+	header: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		marginBottom: 10,
+	},
+	genreWrapper: {
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+	},
+	genre: {
+		margin: 5,
+		padding: 5,
+		backgroundColor: '#eee',
+		borderRadius: 5,
+	},
+});
