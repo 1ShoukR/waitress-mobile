fc8aa4f7bb0f102da07b9a641a86c4f0b720dc09
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-02-09 22:58:26 -0500

    added lastTab lattitude and longigtude to the state

--- redux/authSlice.js
+++ redux/authSlice.js
@@ -14,7 +14,9 @@ const initialState = {
 	userType: null,
 	status: 'idle',
 	error: null,
-	lastTab: 'horses',
+	lastTab: 'HomeTab',
+	latitude: null,
+	longitude: null
 };
 
 const authSlice = createSlice({
@@ -30,7 +32,12 @@ const authSlice = createSlice({
 			state.email = null;
 			state.userType = null;
 			state.authType = null;
+			state.longitude = null;
+			state.latitude = null;
 		},
+		setLastTab: (state, action) => {
+			state.lastTab = action.payload
+		}
         /*
         In theory, this is how we will determine what 
         tab is active when we switch between tabs.
@@ -48,6 +55,7 @@ const authSlice = createSlice({
 	extraReducers(builder) {
 		builder
 			.addCase(doLogin.pending, (state, action) => {
+				console.log('loading')
 				state.status = 'loading';
 			})
 			.addCase(doLogin.fulfilled, (state, action) => {
@@ -61,20 +69,25 @@ const authSlice = createSlice({
 					state.userType = payload.user.type;
 					state.authType = payload.user.auth_type;
 					state.status = 'succeeded';
+					state.latitude = payload.user.latitude
+					state.longitude = payload.user.longitude
+					console.log('teaaaaaa')
 					// Add additional logic if needed for authgroup based on userType or authType
 				} else {
 					state.status = 'failed';
 					state.error = 'Login failed. Please try again.';
+					console.log('NOOOO')
 				}
 			})
 			.addCase(doLogin.rejected, (state, action) => {
 				state.status = 'failed';
 				state.error = action.error.message;
-			});
+				console.error(action.error.message)
+			})
 	},
 });
 
-export const { loggedOut } = authSlice.actions;
+export const { loggedOut, setLastTab } = authSlice.actions;
 export default authSlice.reducer;
 
 export const selectApiToken = (state) => state.auth.apiToken;
