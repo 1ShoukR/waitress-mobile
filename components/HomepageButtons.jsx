e4e5f4a602d7afea6b0fa1afdc4b00b37acc5c4e
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-02-19 09:14:11 -0500

    fixed gap issue on buttons

--- components/HomepageButtons.jsx
+++ components/HomepageButtons.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image,  } from 'react-native';
 import { AntDesign, Fontisto, MaterialCommunityIcons } from '@expo/vector-icons';
 import utensiles from '../assets/utensiles.png';
 import React from 'react';
@@ -6,12 +6,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faCircleInfo, faReceipt } from '@fortawesome/free-solid-svg-icons';
 import { COLORS, FONT, SIZES } from '../constants/theme';
 import Platter from '../assets/platter.svg'
+import { useRouter } from 'expo-router';
 const HomepageButtons = () => {
+	const router = useRouter()
 	return (
 		<View style={styles.columnsContainer}>
 			<View style={styles.column}>
-				<TouchableOpacity style={styles.button}>
+				<TouchableOpacity onPress={() => router.push('/booking/BookingScreen')} style={styles.button}>
 					<View style={styles.buttonContent}>
+						
 						<Text style={styles.buttonText}>Book a Table</Text>
 						{/* <Image style={{ height: 30, width: 30, left: 24, color: COLORS.white }} source={utensiles} /> */}
 						<Platter style={{left: 27}} height={26} width={26} />
@@ -74,10 +77,10 @@ const styles = StyleSheet.create({
 		// backgroundColor: '#DAA520',
 		borderRadius: 5,
 		marginBottom: 10,
-		width: 200,
+		width: 180,
 		height: 60,
 		borderWidth: 1,
-		borderColor: 'rgba(255, 255, 255, 0.6)', // Use a light gray border for subtle contrast
+		borderColor: 'rgba(255, 255, 255, 0.6)', 
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
@@ -87,9 +90,8 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 	},
 	buttonText: {
-		color: COLORS.white, // Ensure text is readable against the primary color
-		fontFamily: FONT.branding, // Apply the branding font
-		fontSize: SIZES.medium, // Use a medium size for button text
+		color: COLORS.white, 
+		fontFamily: FONT.branding, 
+		fontSize: SIZES.medium, 
 	},
-	// Add any specific styles for icons if necessary
 });
