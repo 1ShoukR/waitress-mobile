fbeb8039754f15177c326aa90879a88e69cda142
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-05-10 02:00:02 -0400

    created a new button on home screen to grab a user's location

--- app/(tabs)/home/HomeTab.js
+++ app/(tabs)/home/HomeTab.js
@@ -1,8 +1,9 @@
 import React, { useCallback, useState } from 'react';
-import { RefreshControl, SafeAreaView, ScrollView, View, Text } from 'react-native';
+import { RefreshControl, SafeAreaView, ScrollView, View, Text, TouchableOpacity } from 'react-native';
 import { COLORS } from '../../../constants';
 import { Tabs, Stack } from 'expo-router';
 import HomePageComponent from '../../../components/HomePageComponent';
+import { AntDesign, } from '@expo/vector-icons';
 
 
 
@@ -20,7 +21,16 @@ const HomeIndex = () => {
 			<SafeAreaView style={{ flex: 1, backgroundColor: '#2A2C3B' }}>
 				<Tabs.Screen
 					options={{
-						headerShown: false,
+						headerShown: true,
+						title: 'Waitress',
+						headerBackground: () => <View style={{ backgroundColor: '#2A2C3B', height: 100 }} />,
+						headerRight: () => {
+							return (
+								<TouchableOpacity>
+									<AntDesign style={{ color: COLORS.white }} name="enviromento" size={25} color="black" />
+								</TouchableOpacity>
+							);
+						}
 					}}
 				/>
 				<ScrollView style={{ backgroundColor: '#2A2C3B' }} showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
