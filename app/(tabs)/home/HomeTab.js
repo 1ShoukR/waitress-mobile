56142178a52000dc265967c03035cc456ae59680
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-05-11 02:59:24 -0400

    added basic shell to grab a user's location if it has bot been previously set before

--- app/(tabs)/home/HomeTab.js
+++ app/(tabs)/home/HomeTab.js
@@ -1,17 +1,18 @@
-import React, { useCallback, useState } from 'react';
-import { RefreshControl, SafeAreaView, ScrollView, View, Text, TouchableOpacity } from 'react-native';
+import React, { useCallback, useState, useRef } from 'react';
+import { RefreshControl, SafeAreaView, ScrollView, View, Text, TouchableOpacity, TextInput, Animated, ActivityIndicator } from 'react-native';
 import { COLORS } from '../../../constants';
 import { Tabs, Stack } from 'expo-router';
 import HomePageComponent from '../../../components/HomePageComponent';
-import { AntDesign, } from '@expo/vector-icons';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 import * as Location from 'expo-location';
-
-
-
 
 const HomeIndex = () => {
 	const [refreshing, setRefreshing] = useState(false);
-	const [location, setLocation] = useState(null);
+	const [userLocation, setUserLocation] = useState(null);
+	const [userAddress, setUserAddress] = useState('');
+	const [showAddressInput, setShowAddressInput] = useState(false);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const opacityAnim = useRef(new Animated.Value(1)).current;
 
 	const onRefresh = useCallback(() => {
 		setRefreshing(true);
@@ -19,45 +20,86 @@ const HomeIndex = () => {
 			setRefreshing(false);
 		}, 1500);
 	}, []);
-	const getLocationFromAddress = async (address) => {
-		try {
-			const result = await Location.geocodeAsync(address);
-			if (result.length > 0) {
-				console.log('Latitude:', result[0].latitude, 'Longitude:', result[0].longitude);
+
+	const toggleInput = () => {
+		Animated.timing(opacityAnim, {
+			toValue: 0,
+			duration: 300,
+			useNativeDriver: true,
+		}).start(() => {
+			setShowAddressInput(!showAddressInput);
+			opacityAnim.setValue(0);
+			Animated.timing(opacityAnim, {
+				toValue: 1,
+				duration: 300,
+				useNativeDriver: true,
+			}).start();
+		});
+	};
+
+	const handleAddressSubmit = async () => {
+		setIsLoaded(true); // Start loading
+		if (userAddress) {
+			let results = await Location.geocodeAsync(userAddress);
+			if (results.length > 0) {
+				console.log('Location found:', results[0]);
+				setUserLocation(results[0]); // Assuming the first result is the desired one
+				setShowAddressInput(false);
+				setIsLoaded(false); // Stop loading
 			} else {
-				console.log('No location found for address:', address);
+				console.log('No locations found for the address:', userAddress);
+				setIsLoaded(false); // Stop loading
 			}
-		} catch (error) {
-			console.error('Error getting location:', error);
 		}
-	}
-    return (
-			<SafeAreaView style={{ flex: 1, backgroundColor: '#2A2C3B' }}>
-				<Tabs.Screen
-					options={{
-						headerShown: true,
-						title: '',
-						headerBackground: () => <View style={{ backgroundColor: '#2A2C3B', height: 100 }} />,
-						headerRight: () => (
-							<View style={{ flexDirection: 'row', alignItems: 'center', }}>
-								<Text style={{ color: COLORS.white, fontStyle: 'italic', fontSize: 11 }}>{location ? location : 'No address has been set.'}</Text>
-								<TouchableOpacity onPress={() => console.log('Choose Location')}>
-									<AntDesign name="enviromento" size={25} color={COLORS.white} />
-								</TouchableOpacity>
-							</View>
-						),
-					}}
-				/>
-				<ScrollView style={{ backgroundColor: '#2A2C3B' }} showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
-					<View style={{ flex: 1, paddingVertical: 32, paddingHorizontal: 16 }}>
-						<View style={{ flex: 1 }}>
-							<HomePageComponent refreshing={refreshing} />
-						</View>
-					</View>
-				</ScrollView>
-			</SafeAreaView>
-		);
-}
+	};
 
+	return (
+		<SafeAreaView style={{ flex: 1, backgroundColor: '#2A2C3B' }}>
+			<Tabs.Screen
+				options={{
+					headerShown: true,
+					title: '',
+					headerBackground: () => <View style={{ backgroundColor: '#2A2C3B', height: 100 }} />,
+					headerRight: () => (
+						<TouchableOpacity onPress={toggleInput}>
+							<Animated.View style={{ flexDirection: 'row', alignItems: 'center', opacity: opacityAnim }}>
+								{showAddressInput ? (
+									<>
+										<TextInput
+											style={{ height: 40, borderColor: 'gray', borderWidth: 1, color: COLORS.white, width: 200 }}
+											onChangeText={setUserAddress}
+											value={userAddress}
+											placeholder="Enter Address"
+											placeholderTextColor="grey"
+										/>
+										<TouchableOpacity style={{ backgroundColor: COLORS.primary, padding: 10, borderRadius: 5, marginLeft: 5 }} onPress={handleAddressSubmit}>
+											<Ionicons name="checkmark" size={24} color="black" />
+										</TouchableOpacity>
+									</>
+								) : (
+									<>
+										{isLoaded ? (
+											<ActivityIndicator size="small" color={COLORS.primary} />
+										) : (
+											<Text style={{ color: COLORS.white, fontStyle: 'italic', fontSize: 11 }}>
+												{userLocation ? `Lat: ${userLocation.latitude}, Long: ${userLocation.longitude}` : 'No address has been set.'}
+											</Text>
+										)}
+										<AntDesign name="enviromento" size={25} color={COLORS.white} />
+									</>
+								)}
+							</Animated.View>
+						</TouchableOpacity>
+					),
+				}}
+			/>
+			<ScrollView style={{ backgroundColor: '#2A2C3B' }} showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+				<View style={{ flex: 1, paddingVertical: 32, paddingHorizontal: 16 }}>
+					<HomePageComponent refreshing={refreshing} />
+				</View>
+			</ScrollView>
+		</SafeAreaView>
+	);
+};
 
-export default HomeIndex
+export default HomeIndex;
