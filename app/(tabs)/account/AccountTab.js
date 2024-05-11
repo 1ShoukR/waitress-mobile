e198503f53a2b0d127e58d59f3475516ecb7cb47
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-05-11 01:07:58 -0400

    created AccountTab screen and added screen components as well as styling

--- app/(tabs)/account/AccountTab.js
+++ app/(tabs)/account/AccountTab.js
@@ -1,21 +1,41 @@
-import React, { useCallback, useState } from 'react';
-import { RefreshControl, SafeAreaView, ScrollView, View, Text } from 'react-native';
+import React from 'react';
+import { RefreshControl, SafeAreaView, ScrollView, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { COLORS } from '../../../constants';
 import { Tabs } from 'expo-router';
-
-
-
+import AccountScreenComponent from '../../../components/AccountScreenComponent';
 
 const AccountScreen = () => {
+	return (
+		<SafeAreaView style={styles.safeArea}>
+			<ScrollView style={styles.scrollView} refreshControl={<RefreshControl refreshing={false} onRefresh={() => console.log('Refreshed!')} />}>
+				<AccountScreenComponent />
+				<TouchableOpacity>
+					<View style={styles.logoutButton}>
+						<Text style={{fontWeight: 'bold'}}>Log Out</Text>
+					</View>
+				</TouchableOpacity>
+			</ScrollView>
+		</SafeAreaView>
+	);
+};
 
-    return (
-        <>
-        <View>
-            <Text>account Screen</Text>
-        </View>
-        </>
-    )
-}
+const styles = StyleSheet.create({
+	safeArea: {
+		flex: 1,
+		backgroundColor: '#2A2C3B', 
+	},
+	container: {
+		flex: 1,
+		justifyContent: 'flex-start',
+		alignItems: 'center',
+	},
+    logoutButton: {
+        backgroundColor: COLORS.primary,
+        padding: 20,
+        borderRadius: 10,
+        margin: 20,
+        alignItems: 'center',
+    }
+});
 
-
-export default AccountScreen
+export default AccountScreen;
