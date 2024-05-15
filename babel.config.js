5abd298732abebc8e04e142516e8b0e2fced3de1
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-05-14 23:26:11 -0400

    updated to latest sdk

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,8 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
+module.exports = function (api) {
+	api.cache(true);
+	return {
 		presets: ['babel-preset-expo'],
 		plugins: [
-			'expo-router/babel',
 			[
 				'module:react-native-dotenv',
 				{
