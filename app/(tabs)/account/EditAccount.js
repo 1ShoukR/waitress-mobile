75724f98c232c6d8aece9abc12a492aeffa415b1
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-05-16 01:00:06 -0400

    created a new EditAccount scree

---
+++ app/(tabs)/account/EditAccount.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const EditAccount = () => {
+  return (
+    <>
+      <View>
+        <Text>EditAccount</Text>
+      </View>
+    </>
+  )
+}
+
+export default EditAccount
+
+const styles = StyleSheet.create({})
