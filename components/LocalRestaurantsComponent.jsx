acfdebd49b086aa36e07e125c5610e8a5bfb65fb
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-05-20 03:39:56 -0400

    added dynamic stars for reviews on LocalRestaurants:

--- components/LocalRestaurantsComponent.jsx
+++ components/LocalRestaurantsComponent.jsx
@@ -1,33 +1,51 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { COLORS } from '../constants';
 import React from 'react';
-import { Avatar, Button, Card } from 'react-native-paper';
+import { Card } from 'react-native-paper';
 import { router } from 'expo-router';
+import { FontAwesome } from '@expo/vector-icons';
+
 
 const LocalRestaurantsComponent = ({ localRestaurants }) => {
-	const localRestaurantsData = localRestaurants?.localRestaurants?.restaurants;
-	console.log('Local Restaurants:', localRestaurants);
+	const localRestaurantsData = localRestaurants?.localRestaurants;
+	console.log('Local Restaurants:', localRestaurantsData);
 
 	const handlePress = (restaurant) => {
 		// Handle the card click event here
 		console.log('Restaurant clicked:', restaurant.Name);
 		router.push(`/home/restaurant/${restaurant.RestaurantId}`);
 	};
+	const renderStars = (rating) => {
+		const stars = [];
+		for (let i = 1; i <= 5; i++) {
+			stars.push(<FontAwesome key={i} name={i <= rating ? 'star' : 'star-o'} size={18} color={COLORS.black} />);
+		}
+		return stars;
+	};
 
 	return (
 		<View style={styles.container}>
 			<Text style={styles.headerText}>Restaurants Near You</Text>
-			{localRestaurantsData?.map((restaurant, index) => (
-				<TouchableOpacity key={index} onPress={() => handlePress(restaurant)} style={styles.cardContainer}>
-					<Card style={styles.card}>
-						<Card.Cover source={{ uri: restaurant?.ImageURL }} style={styles.cardImage} />
-						<Card.Content>
-							<Text style={styles.restaurantName}>{restaurant?.Name}</Text>
-							<Text style={styles.restaurantDetails}>{restaurant?.Details}</Text>
-						</Card.Content>
-					</Card>
-				</TouchableOpacity>
-			))}
+			{localRestaurantsData?.map((restaurant, index) => {
+				console.log('Restaurant:', restaurant.Ratings);
+				return (
+					<TouchableOpacity key={index} onPress={() => handlePress(restaurant)} style={styles.cardContainer}>
+						<Card style={styles.card}>
+							<Card.Cover source={{ uri: restaurant?.ImageURL }} style={styles.cardImage} />
+							<Card.Content>
+								<Text style={styles.restaurantName}>{restaurant?.Name}</Text>
+								<View style={styles.ratingContainer}>
+									{renderStars(restaurant?.Ratings.reduce((sum, review) => sum + review.Rating, 0) / restaurant.Ratings.length)}
+									<Text style={styles.ratingText}>{restaurant?.Rating}</Text>
+								</View>
+								<Text style={styles.restaurantTags}>Tags</Text>
+								<Text style={styles.restaurantDetails}>{restaurant?.Details}</Text>
+							</Card.Content>
+						</Card>
+					</TouchableOpacity>
+				);
+			})}
+			
 		</View>
 	);
 };
@@ -57,6 +75,11 @@ const styles = StyleSheet.create({
 		height: 150,
 		borderTopLeftRadius: 10,
 		borderTopRightRadius: 10,
+	},
+	ratingContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginBottom: 5,
 	},
 	restaurantName: {
 		fontSize: 18,
