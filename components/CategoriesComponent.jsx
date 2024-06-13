ce1a3b1ed15afe37f34a3529336d709a691b1a7e
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-06-13 00:37:32 -0400

    added new router push for category page

--- components/CategoriesComponent.jsx
+++ components/CategoriesComponent.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, ScrollView, Image, TouchableOpacity } from 'react-native';
 import React from 'react'
 import { COLORS } from '../constants';
+import { router } from 'expo-router';
 
 const CategoriesComponent = ({foodCategories}) => {
   return (
@@ -9,13 +10,13 @@ const CategoriesComponent = ({foodCategories}) => {
 			<ScrollView horizontal={true}>
 				<View style={styles.genreWrapper}>
 					{foodCategories.map((category, index) => (
-						<TouchableOpacity onPress={() => console.log(`${category.CategoryName} Category Pressed`)} key={index} style={styles.genre}>
+						<TouchableOpacity onPress={() => router.push(`/home/category/${category.CategoryID}`)} key={index} style={styles.genre}>
 							{/* Need to add SVG images of categories, 
 							then remove background color and have 
 							just the image and text 
 							*/}
 							<Image source={{ uri: category.ImageUrl }} />
-							<Text style={{fontWeight: 'bold'}}>{category.CategoryName}</Text>
+							<Text style={{ fontWeight: 'bold' }}>{category.CategoryName}</Text>
 						</TouchableOpacity>
 					))}
 				</View>
