71c1ea11740ae0e3977cb69910fc6dbcbad992bf
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-06-27 23:34:49 -0400

    created a new component for an individual menu item

---
+++ components/MenuItemComponents/IndividualMenuItem.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const IndividualMenuItem = () => {
+  return (
+    <View>
+      <Text>IndividualMenuItem</Text>
+    </View>
+  )
+}
+
+export default IndividualMenuItem
+
+const styles = StyleSheet.create({})
