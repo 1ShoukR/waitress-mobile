0443dc961d766a2e37a700d869955378648ec989
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-07-05 15:42:39 -0400

    added new header details to order menu

--- app/(tabs)/home/order/ViewOrderScreen.js
+++ app/(tabs)/home/order/ViewOrderScreen.js
@@ -1,11 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React from 'react'
+import { router, Stack } from 'expo-router'
+import { COLORS } from '../../../../constants'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 const ViewOrderScreen = () => {
   return (
+    <>
+    <Stack.Screen 
+    options={{
+        headerTitle: 'Your Order',
+        headerStyle: {
+            backgroundColor: COLORS.primary
+        },
+        headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+                <FontAwesomeIcon size={24} icon={faArrowLeft} />
+            </TouchableOpacity>
+        )
+    }}
+    />
     <View>
       <Text>ViewOrderScreen</Text>
     </View>
+    </>
   )
 }
 
