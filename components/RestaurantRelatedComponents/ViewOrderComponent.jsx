21900a3e8b799f1e72ed4b044c12db509c0b0361
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-07-08 23:50:43 -0400

    added user name and restaurant that has the order selected

--- components/RestaurantRelatedComponents/ViewOrderComponent.jsx
+++ components/RestaurantRelatedComponents/ViewOrderComponent.jsx
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const ViewOrderComponent = () => {
-  return (
-    <View>
-      <Text>ViewOrderComponent</Text>
-    </View>
-  )
-}
+	const userOrder = useSelector((state) => state?.orders?.order?.[0]);
+	const user = useSelector((state) => state?.auth);
 
-export default ViewOrderComponent
+	// Extract restaurant name safely
+	const restaurantName = userOrder?.restaurant?.Name || 'Unknown Restaurant';
 
-const styles = StyleSheet.create({})
+	console.log('user', user);
+	console.log('userOrder', userOrder);
+
+	return (
+		<View style={styles.checkoutHeader}>
+			<Text>{`${user.firstName},`}</Text>
+			<Text>{`your order for ${restaurantName}`}</Text>
+		</View>
+	);
+};
+
+export default ViewOrderComponent;
+
+const styles = StyleSheet.create({
+	checkoutHeader: {
+		flexDirection: 'column',
+		// justifyContent: 'space-between',
+		padding: 16,
+		// backgroundColor: '#f8f8f8',
+	},
+});
