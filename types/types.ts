b57ae0ddaae3da40a2e7b2b287b3f5e6237fdf57
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-07-15 00:34:11 -0400

    created LoginRequestData and Response, along with User interface

--- types/types.ts
+++ types/types.ts
@@ -2,3 +2,29 @@ export interface ClientConfig {
     endpoint: string;
     
 }
+
+export type User = {
+    userId: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    authType: string;
+    latitude: number;
+    longitude: number;
+    address: string;
+    userType: string;
+    createdAt: string;
+    darkMode: boolean;
+}
+export interface LoginRequestData {
+	email: string;
+	password: string;
+	userAgent: string;
+}
+
+
+export interface LoginResponse {
+    token: string
+    apiToken: string
+    user: User
+}
