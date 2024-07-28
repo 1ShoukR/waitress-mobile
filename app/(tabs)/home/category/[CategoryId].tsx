801eb3be30404e1065662ff6cd772012ddbaa414
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-07-28 19:11:27 -0400

    typed

--- app/(tabs)/home/category/[CategoryId].tsx
+++ app/(tabs)/home/category/[CategoryId].tsx
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React, {useEffect} from 'react'
 import { useLocalSearchParams } from 'expo-router'
 
-const CategoryScreen = () => {
+const CategoryScreen = (): React.JSX.Element => {
     const { CategoryId } = useLocalSearchParams();
     console.log('CategoryId', CategoryId);
     useEffect(() => {
