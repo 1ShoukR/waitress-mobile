6c37a86b49c569d1fa4b84709cdf87aa22890eaf
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-08-06 23:24:56 -0400

    fetching and displaying new restaurant data

--- app/(tabs)/home/category/[CategoryId].tsx
+++ app/(tabs)/home/category/[CategoryId].tsx
@@ -1,17 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, ScrollView } from 'react-native'
 import React, {useEffect} from 'react'
 import { useLocalSearchParams } from 'expo-router'
+import { RestaurantCategoryList } from '@components/CategoryScreenComponent/RestaurantCategoryList';
+import client from 'api/client';
+import { useAppSelector } from 'redux/hooks';
+import { Restaurant } from 'types/types';
 
 const CategoryScreen = (): React.JSX.Element => {
     const { CategoryId } = useLocalSearchParams();
+    const [restaurants, setRestaurants] = React.useState<Restaurant[]>([]);
+    const token = useAppSelector((state) => state.auth.token);
     console.log('CategoryId', CategoryId);
     useEffect(() => {
-      console.log('fetch for all category items')
+      async function fetchData() {
+        const response = await client.post<Restaurant[]>(`/api/category/${CategoryId}/restaurants`, null, token);
+        setRestaurants(response);
+      }
+      fetchData()
     },[])
+    console.log('restaurants', restaurants);
   return (
-    <View>
-      <Text>CategoryScreen</Text>
-    </View>
+    <ScrollView>
+      <RestaurantCategoryList restaurants={restaurants} />
+    </ScrollView>
   )
 }
 
