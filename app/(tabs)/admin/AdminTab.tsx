e645c3c3b010d1218b8f593198f319bac57d6d6b
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-09-02 20:01:46 -0400

    feat: basic fetch for admin tab

--- app/(tabs)/admin/AdminTab.tsx
+++ app/(tabs)/admin/AdminTab.tsx
@@ -1,13 +1,28 @@
-import React from "react";
-import {View, Text} from "react-native";
+import client from "api/client";
+import React, { useEffect } from "react";
+import {View, Text, TouchableOpacity} from "react-native";
+import { useAppSelector } from "redux/hooks";
 
 
 const AdminTab = (): React.JSX.Element => {
-    return (
-        <View>
-            <Text>Admin Tab</Text>
-        </View>
-    )
+    const token = useAppSelector((state) => state?.auth?.apiToken);
+    console.log('token', token)
+    const fetchData = async () => {
+        try {
+            const data = await client.post('/api/users/get-admin-data', token);
+            console.log('data', data);
+        } catch (error) {
+            console.log('error', error);
+        }
+    }
+    return ( 
+			<View>
+				<Text>Admin Tab</Text>
+				<TouchableOpacity onPress={fetchData}>
+					<Text>Fetch Data</Text>
+				</TouchableOpacity>
+			</View>
+		);
 }
 
 export default AdminTab;
