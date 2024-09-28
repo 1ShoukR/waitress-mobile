849685020be4cec246209b0d1c9ed6ebf6ff7bb8
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-09-28 01:32:07 -0400

    update for three js

--- metro.config.js
+++ metro.config.js
@@ -1,26 +1,26 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 module.exports = (() => {
-	const config = getDefaultConfig(__dirname);
+  const config = getDefaultConfig(__dirname);
 
-	const { transformer, resolver } = config;
+  const { transformer, resolver } = config;
 
-	config.transformer = {
-		...transformer,
-		babelTransformerPath: require.resolve('react-native-svg-transformer'),
-		getTransformOptions: async () => ({
-			transform: {
-				experimentalImportSupport: false,
-				inlineRequires: false,
-			},
-		}),
-	};
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  };
 
-	config.resolver = {
-		...resolver,
-		assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-		sourceExts: [...resolver.sourceExts, 'svg', 'ts', 'tsx'], // Add 'ts' and 'tsx' extensions
-	};
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg').concat(['glb', 'gltf', 'png', 'jpg']), // Add additional asset extensions
+    sourceExts: [...resolver.sourceExts, 'svg', 'ts', 'tsx', 'js', 'jsx', 'json', 'cjs', 'mjs'], // Add additional source extensions
+  };
 
-	return config;
+  return config;
 })();
