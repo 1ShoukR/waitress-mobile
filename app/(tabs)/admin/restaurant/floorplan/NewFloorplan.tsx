3eec5ee00fef317dfcf9fce96681b6b61a9da0de
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-09-28 01:50:50 -0400

    feat: added plus and x button

--- app/(tabs)/admin/restaurant/floorplan/NewFloorplan.tsx
+++ app/(tabs)/admin/restaurant/floorplan/NewFloorplan.tsx
@@ -1,19 +1,22 @@
+import React, { useState, useRef } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+import { Canvas } from "@react-three/fiber/native";
+import { OrthographicCamera } from "@react-three/drei/native";
+import { useFrame } from "@react-three/fiber/native";
+import { OrthographicCamera as ThreeOrthographicCamera, Vector3 } from "three";
+import { COLORS } from "../../../../../constants";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faCheck, faPlus, faX } from "@fortawesome/free-solid-svg-icons";
 
-
-
-import React, { useState, useRef } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
-import { Canvas } from '@react-three/fiber/native';
-import { OrthographicCamera } from '@react-three/drei/native';
-import { useFrame } from '@react-three/fiber/native';
-import {  OrthographicCamera as ThreeOrthographicCamera, Vector3 } from 'three';
-import { COLORS } from '../../../../../constants';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faCheck, faPlus } from '@fortawesome/free-solid-svg-icons';
-
-
-  const Grid = ({ size, divisions}: {size: number, divisions: number;}) => {
-  const stretchedSize = size * 1.3; 
+const Grid = ({ size, divisions }: { size: number; divisions: number }) => {
+  const stretchedSize = size * 1.3;
   return (
     <gridHelper
       args={[stretchedSize, divisions, COLORS.black, COLORS.gray]}
@@ -22,7 +25,6 @@ import { faCheck, faPlus } from '@fortawesome/free-solid-svg-icons';
     />
   );
 };
-
 
 const Scene = () => {
   const cameraRef = useRef<ThreeOrthographicCamera>(null);
@@ -49,41 +51,54 @@ const Scene = () => {
 };
 
 const NewFloorplanScreen = () => {
-  const [floorPlanName, setFloorPlanName] = useState<string>('');
+  const [floorPlanName, setFloorPlanName] = useState<string>("");
+  const [showToolbar, setShowToolbar] = useState<boolean>(false);
 
   const handleSave = () => {
     console.log(`Saved: ${floorPlanName}`);
     // Implement your save logic here
   };
 
-  const handleToolbarPress = () =>{
-    console.log('Toolbar pressed');
-  }
+  const handleToolbarPress = () => {
+    console.log("Toolbar pressed");
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerText}>New Floorplan</Text>
 
-          <TextInput
-            style={styles.input}
-            value={floorPlanName}
-            onChangeText={setFloorPlanName}
-            placeholder="Floor Plan Name"
-          />
+        <TextInput
+          style={styles.input}
+          value={floorPlanName}
+          onChangeText={setFloorPlanName}
+          placeholder="Floor Plan Name"
+        />
         <View style={styles.inputContainer}>
           <TouchableOpacity style={styles.checkButton} onPress={handleSave}>
             <FontAwesomeIcon icon={faCheck} color="#fff" size={15} />
           </TouchableOpacity>
         </View>
       </View>
-            
+
       <Canvas style={styles.canvas}>
-        <Scene  />
+        <Scene />
       </Canvas>
-      <TouchableOpacity style={styles.floatingButton} onPress={handleToolbarPress}>
-        <FontAwesomeIcon icon={faPlus} color="#fff" size={20} />
-      </TouchableOpacity>
+      {!showToolbar ? (
+        <TouchableOpacity
+          style={styles.floatingButton}
+          onPress={() => setShowToolbar(true)}
+        >
+          <FontAwesomeIcon icon={faPlus} color="#fff" size={20} />
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity
+          style={[styles.floatingButton, { backgroundColor: COLORS.danger }]}
+          onPress={() => setShowToolbar(false)}
+        >
+          <FontAwesomeIcon icon={faX} color="#fff" size={20} />
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
@@ -91,10 +106,10 @@ const NewFloorplanScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F0F0F0',
+    backgroundColor: "#F0F0F0",
   },
   floatingButton: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 20,
     right: 20,
     backgroundColor: COLORS.success,
@@ -103,49 +118,48 @@ const styles = StyleSheet.create({
   },
   header: {
     padding: 16,
-    backgroundColor: '#FFFFFF',
-    flexDirection: 'row',
-    gap: 5
+    backgroundColor: "#FFFFFF",
+    flexDirection: "row",
+    gap: 5,
   },
   headerText: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 16,
   },
   inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   input: {
     flex: 1,
     height: 40,
     borderWidth: 1,
-    borderColor: '#CCCCCC',
+    borderColor: "#CCCCCC",
     borderRadius: 5,
     paddingHorizontal: 10,
     marginRight: 10,
-    backgroundColor: '#FFFFFF',
-    },
+    backgroundColor: "#FFFFFF",
+  },
   saveButton: {
-    backgroundColor: '#4CAF50',
+    backgroundColor: "#4CAF50",
     padding: 10,
     borderRadius: 5,
   },
-    checkButton: {
-    backgroundColor: '#4CAF50',
+  checkButton: {
+    backgroundColor: "#4CAF50",
     padding: 8,
     borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   saveButtonText: {
-    color: '#FFFFFF',
-    fontWeight: 'bold',
+    color: "#FFFFFF",
+    fontWeight: "bold",
   },
   canvas: {
     flex: 1,
   },
 });
-
 
 export default NewFloorplanScreen;
