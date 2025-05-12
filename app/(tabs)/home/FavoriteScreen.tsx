cf1eeebd9f08641b405d0fe533f3da325e900d51
1ShoukR <rahminshoukoohi@gmail.com>
2025-05-11 21:36:59 -0400

    created new favorites screen

--- app/(tabs)/home/FavoriteScreen.tsx
+++ app/(tabs)/home/FavoriteScreen.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { View, Text, SafeAreaView, Button, StyleSheet } from 'react-native';
 import { Stack, useNavigation } from 'expo-router';
+import client from 'api/client';
 
 const FavoriteScreen = (): React.JSX.Element => {
+    const [favorites, setFavorites] = useState<any[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchFavorites = async () => {
+            try {
+                const response = await client.get('/api/favorites');
+                setFavorites(response.data as any[]);
+            } catch (error) {
+                setError(error as string);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetchFavorites();
+    }, []);
+
     const navigation = useNavigation()
     return (
         <SafeAreaView>
